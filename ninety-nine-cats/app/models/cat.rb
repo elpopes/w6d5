1971ffc9df5ea43b8afb3c1ede07cc132ebde422
Timothy Chang <tchang135@gmail.com>
2022-12-16 16:57:09 -0500

    figuring out our cat.rb model

--- ninety-nine-cats/app/models/cat.rb
+++ ninety-nine-cats/app/models/cat.rb
@@ -1,7 +1,38 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+require 'action_view'
+
 class Cat < ApplicationRecord
+    # include DateHelper
+    include ActionView::Helpers::DateHelper
     CAT_COLORS = [:white, :black, :grey, :orange, :hairless, :brown ]
+    validates :birth_date, :name, presence: true
+    validates :color, presence: true, inclusion: {in: CAT_COLORS,  message: "%{value} is not a valid color" }
+    validates :sex, presence: true, inclusion: {in: %w(M F),  message: "%{value} is not a valid sex" }
+    validate :birth_date_cannot_be_future
 
-    validates :birth_date, :name, :sex, presence :true
-    validate :color, presence :true, inclusion: {in: CAT_COLORS }
+    def birth_date_cannot_be_future
+        kitty = self
+        if kitty.birth_date && kitty.birth_date > Date.today
+            errors.add(:birth_date, "can't be in the future")
+        end
+    end
 
+
+    def age 
+        time_ago_in_words(birth_date.years.from_now)
+    end
+
+    
 end
