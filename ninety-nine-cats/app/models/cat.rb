a83404ea96c6d9bbbff5ef3a1b8a43233ccef70c
Timothy Chang <tchang135@gmail.com>
2022-12-16 17:14:32 -0500

    finished cat.rb

--- ninety-nine-cats/app/models/cat.rb
+++ ninety-nine-cats/app/models/cat.rb
@@ -14,17 +14,16 @@
 require 'action_view'
 
 class Cat < ApplicationRecord
-    # include DateHelper
     include ActionView::Helpers::DateHelper
-    CAT_COLORS = [:white, :black, :grey, :orange, :hairless, :brown ]
+    CAT_COLORS = ['white', 'black', 'grey', 'orange', 'hairless', 'brown' ]
     validates :birth_date, :name, presence: true
     validates :color, presence: true, inclusion: {in: CAT_COLORS,  message: "%{value} is not a valid color" }
     validates :sex, presence: true, inclusion: {in: %w(M F),  message: "%{value} is not a valid sex" }
     validate :birth_date_cannot_be_future
 
     def birth_date_cannot_be_future
-        kitty = self
-        if kitty.birth_date && kitty.birth_date > Date.today
+        # debugger
+        if birth_date.is_a?(Date) && birth_date > Date.today
             errors.add(:birth_date, "can't be in the future")
         end
     end
